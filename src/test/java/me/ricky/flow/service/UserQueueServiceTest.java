56877010607be1c2a4455be797b12548b52d1d11
RichardKim <hiddeneventor@gmail.com>
2024-01-19 22:57:17 +0900

    [Test] 접근 가능 여부 테스트 코드 작성 및 테스트 완료

--- src/test/java/me/ricky/flow/service/UserQueueServiceTest.java
+++ src/test/java/me/ricky/flow/service/UserQueueServiceTest.java
@@ -1,21 +1,33 @@
 package me.ricky.flow.service;
 
 import me.ricky.flow.EmbeddedRedis;
+import me.ricky.flow.exception.ApplicationException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.ReactiveRedisConnection;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Import(EmbeddedRedis.class) //
 class UserQueueServiceTest {
     @Autowired
-    private  UserQueueService userQueueService;
+    private UserQueueService userQueueService;
     @Autowired
     private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    @BeforeEach
+    void beforeEach() {
+        ReactiveRedisConnection redisConnection = reactiveRedisTemplate.getConnectionFactory().getReactiveConnection();
+        redisConnection.serverCommands().flushAll().subscribe();
+    }
+
 
     @Test
     void registerWaitQueue() {
@@ -31,10 +43,61 @@ class UserQueueServiceTest {
     }
 
     @Test
+    void registerWaitQueueException() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 100L))
+                )
+                .expectError(ApplicationException.class)
+                .verify();
+    }
+
+    @Test
+    void emptyAllowUser() {
+        StepVerifier.create(userQueueService.allowUser("default", 3L))
+                .expectNext(0L)
+                .verifyComplete();
+    }
+
+    @Test
     void allowUser() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 2L))
+                )
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void allowUserAfterRegisterWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.registerWaitQueue("default", 200L))
+                )
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void isNotAllowed() {
+        StepVerifier.create(userQueueService.isAllowed("default", 100L))
+                .expectNext(false)
+                .verifyComplete();
     }
 
     @Test
     void isAllowed() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.allowUser("default", 1L))
+                        .then(userQueueService.isAllowed("default", 100L))
+                )
+                .expectNext(true)
+                .verifyComplete();
     }
+
+
 }

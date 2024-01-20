d15ca8c2be602f3116c29e8edbb7f07dbcae80d9
RichardKim <hiddeneventor@gmail.com>
2024-01-20 21:55:10 +0900

    [Schedule] 주기적 접속 허용 처리 - 3초마다 3명씩 접속 허용

--- src/main/java/me/ricky/flow/service/UserQueueService.java
+++ src/main/java/me/ricky/flow/service/UserQueueService.java
@@ -3,18 +3,23 @@ package me.ricky.flow.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.ricky.flow.exception.ErrorCode;
+import org.springframework.data.redis.connection.zset.Tuple;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "user:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "user:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "user:queue:%s:proceed";
 
 
@@ -58,6 +63,18 @@ public class UserQueueService {
 
     @Scheduled(initialDelay = 5000, fixedDelay = 3000)
     public void scheduleAllowUser() {
-        log.info("scheduleAllowUser");
+        log.info("called scheduling...");
+        var maxAllowUserCount = 3L;
+        // 사용자 접속 허용 처리
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build()
+                )
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("maxAllowUserCount {} allowed {} users {}", maxAllowUserCount, tuple.getT2(), tuple.getT1()))
+                .subscribe();
+
     }
 }

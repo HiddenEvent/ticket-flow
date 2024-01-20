d7566b8756a94a18b5fb169b4d0704f6555190e6
RichardKim <hiddeneventor@gmail.com>
2024-01-20 22:04:08 +0900

    [Schedule] 설정 수정 - test 에서 schedule 실행 제어를 위해..

--- src/main/java/me/ricky/flow/FlowApplication.java
+++ src/main/java/me/ricky/flow/FlowApplication.java
@@ -2,9 +2,7 @@ package me.ricky.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class FlowApplication {
     public static void main(String[] args) {
